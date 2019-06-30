7bbb4566a499272383a92ff4fd1a8a2bf5633cc2
wangpf <wangpf2011@163.com>
2019-06-30 10:12:36 +0800

    init commit

--- jeeopen-common/jeeopen-common-oss/src/main/java/com/jeeopen/common/oss/client/AbstractOSSClient.java
+++ jeeopen-common/jeeopen-common-oss/src/main/java/com/jeeopen/common/oss/client/AbstractOSSClient.java
@@ -2,11 +2,11 @@ package com.jeeopen.common.oss.client;
 
 /**
  * All rights Reserved, Designed By kjt.gzst.gov.cn
- *
+ * 
  * @version V1.0
  * @title: IOSSClient.java
  * @description: 操作客户端虚类
-* @author: wangpf
+ * @author: wangpf
  * @date: 2017年8月11日 下午9:55:00
  * @copyright: 2017 kjt.gzst.gov.cn Inc. All rights reserved.
  */
